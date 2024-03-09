e4a31a617c4d0df5a6523c17819d3687e3978e00
Gouranga Satapathy <pulusatapathy@gmail.com>
2024-03-09 13:19:25 +0530

    small refactor

--- src/main/java/com/ultimateScraper/scrape/ServiceImpl/AsyncServiceImpl.java
+++ src/main/java/com/ultimateScraper/scrape/ServiceImpl/AsyncServiceImpl.java
@@ -22,77 +22,80 @@ import com.ultimateScraper.scrape.utilities.GenericService;
 
 @Service
 public class AsyncServiceImpl implements AsyncService {
-	private static final Logger logger = LogManager.getLogger(AsyncServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(AsyncServiceImpl.class);
 
-	@Value("${filter.content}")
-	private String filterContent;
+    @Value("${filter.content}")
+    private String filterContent;
 
-	private final GenericService genericService;
-	private final ExternalApiService externalApiService;
+    private final GenericService genericService;
+    private final ExternalApiService externalApiService;
 
-	public AsyncServiceImpl(GenericService genericService, ExternalApiService externalApiService) {
-		this.genericService = genericService;
-		this.externalApiService = externalApiService;
-	}
+    public AsyncServiceImpl(GenericService genericService, ExternalApiService externalApiService) {
+        this.genericService = genericService;
+        this.externalApiService = externalApiService;
+    }
 
-	/*
-	 * @Override public CompletableFuture<List<GenericApiResp>>
-	 * invokeGetAllRes(RequestBodyParam searchTerm) { if
-	 * (genericService.readTextFile(searchTerm.getInputQuery())) { return
-	 * CompletableFuture.failedFuture(new FilterContent(filterContent)); }
-	 * 
-	 * List<CompletableFuture<List<GenericApiResp>>> apiCalls = new ArrayList<>();
-	 * 
-	 * for (String val : searchTerm.getSource()) {
-	 * CompletableFuture<List<GenericApiResp>> apiCall =
-	 * getApiCall(searchTerm.getInputQuery(), val);
-	 * 
-	 * apiCalls.add(apiCall.exceptionally(ex -> { return Collections.emptyList();
-	 * }));
-	 * 
-	 * } CompletableFuture<Void> allOf =
-	 * CompletableFuture.allOf(apiCalls.toArray(new CompletableFuture[0]));
-	 * 
-	 * return allOf.thenApply( v ->
-	 * apiCalls.stream().map(CompletableFuture::join).flatMap(List::stream).collect(
-	 * Collectors.toList())); }
-	 */
-	
-	@Override
-	public List<GenericApiResp> invokeGetAllRes(RequestBodyParam searchTerm) {
+    /*
+     * @Override public CompletableFuture<List<GenericApiResp>>
+     * invokeGetAllRes(RequestBodyParam searchTerm) { if
+     * (genericService.readTextFile(searchTerm.getInputQuery())) { return
+     * CompletableFuture.failedFuture(new FilterContent(filterContent)); }
+     *
+     * List<CompletableFuture<List<GenericApiResp>>> apiCalls = new ArrayList<>();
+     *
+     * for (String val : searchTerm.getSource()) {
+     * CompletableFuture<List<GenericApiResp>> apiCall =
+     * getApiCall(searchTerm.getInputQuery(), val);
+     *
+     * apiCalls.add(apiCall.exceptionally(ex -> { return Collections.emptyList();
+     * }));
+     *
+     * } CompletableFuture<Void> allOf =
+     * CompletableFuture.allOf(apiCalls.toArray(new CompletableFuture[0]));
+     *
+     * return allOf.thenApply( v ->
+     * apiCalls.stream().map(CompletableFuture::join).flatMap(List::stream).collect(
+     * Collectors.toList())); }
+     */
+
+    @Override
+    public List<GenericApiResp> invokeGetAllRes(RequestBodyParam searchTerm) {
+		List<CompletableFuture<List<GenericApiResp>>> apiCallFutures = new ArrayList<>();
 
 		try {
 			if (genericService.readTextFile(searchTerm.getInputQuery())) {
 				throw new FilterContent(filterContent);
 			}
-
 		} catch (IOException e) {
 			logger.error("Error at: {} {}", "invokeGetAllRes", e.getMessage());
 		}
 
-		List<List<GenericApiResp>> apiCalls = new ArrayList<>();
+        for (String val : searchTerm.getSource()) {
+            CompletableFuture<List<GenericApiResp>> apiCallFuture = CompletableFuture.supplyAsync(() ->
+                    getApiCall(searchTerm.getInputQuery(), val));
+            apiCallFutures.add(apiCallFuture);
+        }
 
-		for (String val : searchTerm.getSource()) {
-			List<GenericApiResp> apiCall = getApiCall(searchTerm.getInputQuery(), val);
-			apiCalls.add(apiCall);
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(apiCallFutures.toArray(new CompletableFuture[0]));
 
-		}
+        return allOf.thenApply(v ->
+                        apiCallFutures.stream()
+                                .map(CompletableFuture::join)
+                                .flatMap(List::stream)
+                                .collect(Collectors.toList()))
+                .join();
+    }
 
-		return apiCalls.stream().map(e->e).flatMap(List::stream).collect(Collectors.toList());
-	}
-
-	private List<GenericApiResp> getApiCall(String inputQuery, String apiName) {
-		if (apiName.equalsIgnoreCase("yts")) {
-			return externalApiService.getCachedYtsRes(inputQuery);
-		}
-		else if (apiName.equalsIgnoreCase("piratebay")) {
-			return externalApiService.getCachedpirateBayRes(inputQuery);
-		}
-		else if (apiName.equalsIgnoreCase("snowfl")) {
-			return externalApiService.getCachedSnowFlRes(inputQuery);
-		}
-		// Add conditions for other APIs  in the future
-		return Collections.emptyList();
-	}
+    private List<GenericApiResp> getApiCall(String inputQuery, String apiName) {
+        if (apiName.equalsIgnoreCase("yts")) {
+            return externalApiService.getCachedYtsRes(inputQuery);
+        } else if (apiName.equalsIgnoreCase("piratebay")) {
+            return externalApiService.getCachedpirateBayRes(inputQuery);
+        } else if (apiName.equalsIgnoreCase("snowfl")) {
+            return externalApiService.getCachedSnowFlRes(inputQuery);
+        }
+        // Add conditions for other APIs  in the future
+        return Collections.emptyList();
+    }
 
 }
